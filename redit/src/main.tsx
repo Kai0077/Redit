48eba62d1e3a827f4bb61e623f4190a099eb7281
KaiSej007 <kailyubo@gmail.com>
2025-10-13 11:51:24 +0200

    added startup

--- redit/src/main.tsx
+++ redit/src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import 'primereact/resources/themes/lara-dark-blue/theme.css'; // or another theme
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css'; // for surface-, flex, gap-, border-round-*
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
