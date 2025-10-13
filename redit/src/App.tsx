98d351f511feacdc8029be8c29d01a9441c39ad1
Wei Yang <wei0038@proton.me>
2025-10-13 11:37:28 +0200

    init redit client frontend structure

---
+++ redit/src/App.tsx
@@ -0,0 +1,14 @@
+import Menubar from "./layouts/Menubar";
+import './App.css'
+
+function App() {
+    return (
+        <div className="min-h-screen">
+            <Menubar>
+                
+            </Menubar>
+        </div>
+    );
+}
+
+export default App
