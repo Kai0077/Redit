3c97535617b18844b5cb5a753fa80e0376fdb7cc
Wei Yang <wei0038@proton.me>
2025-10-15 17:03:29 +0200

    added menubar into adminpage

--- redit/src/pages/AdminPage.tsx
+++ redit/src/pages/AdminPage.tsx
@@ -1,42 +1,46 @@
-import { TabView, TabPanel } from "primereact/tabview";
-import { useState } from "react";
+import {TabView, TabPanel} from "primereact/tabview";
+import {useState} from "react";
 import PostList from "../components/posts/AdminPostList";
 import AppToast from "../components/AppToast";
 import "../../styles/AdminPage.css";
+import Menubar from "../layouts/Menubar.tsx";
 
 export default function AdminPage() {
     const [activeIndex, setActiveIndex] = useState(0);
 
     return (
-        <div className="admin-page">
-            <AppToast />
-            <h1 className="admin-title">Admin Dashboard</h1>
+        <div>
+            <Menubar/>
+            <div className="admin-page">
+                <AppToast/>
+                <h1 className="admin-title">Admin Dashboard</h1>
 
-            <div className="admin-tabs">
-                <TabView
-                    activeIndex={activeIndex}
-                    onTabChange={(e) => setActiveIndex(e.index)}
-                    className="custom-tabview"
-                >
-                    {/* Posts Tab */}
-                    <TabPanel header="Posts">
-                        <PostList />
-                    </TabPanel>
+                <div className="admin-tabs">
+                    <TabView
+                        activeIndex={activeIndex}
+                        onTabChange={(e) => setActiveIndex(e.index)}
+                        className="custom-tabview"
+                    >
+                        {/* Posts Tab */}
+                        <TabPanel header="Posts">
+                            <PostList/>
+                        </TabPanel>
 
-                    {/* Communities Tab */}
-                    <TabPanel header="Communities">
-                        <div className="tab-placeholder">
-                            <p>Community management coming soon.</p>
-                        </div>
-                    </TabPanel>
+                        {/* Communities Tab */}
+                        <TabPanel header="Communities">
+                            <div className="tab-placeholder">
+                                <p>Community management coming soon.</p>
+                            </div>
+                        </TabPanel>
 
-                    {/* Users Tab */}
-                    <TabPanel header="Users">
-                        <div className="tab-placeholder">
-                            <p>User management coming soon.</p>
-                        </div>
-                    </TabPanel>
-                </TabView>
+                        {/* Users Tab */}
+                        <TabPanel header="Users">
+                            <div className="tab-placeholder">
+                                <p>User management coming soon.</p>
+                            </div>
+                        </TabPanel>
+                    </TabView>
+                </div>
             </div>
         </div>
     );
