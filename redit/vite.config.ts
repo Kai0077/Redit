62eb57a450406bf35e27c7dfa16d4999dc406e13
Wei Yang <wei0038@proton.me>
2025-12-05 17:52:47 +0100

    fix allowed

--- redit/vite.config.ts
+++ redit/vite.config.ts
@@ -1,21 +1,16 @@
-import { defineConfig, loadEnv } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    plugins: [react()],
+    return {
+        plugins: [react()],
 
-    server: {
-      host: true,
-    },
-
-    preview: {
-      allowedHosts: [
-        env.VITE_ALLOWED_HOST,
-        env.VITE_LOCAL_ALLOWED_HOST,
-      ].filter(Boolean),
-    },
-  };
+        preview: {
+            allowedHosts: [
+                env.VITE_ALLOWED_HOST,
+            ],
+        },
+    };
 });
